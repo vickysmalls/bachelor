220d8b8bab97d46a1eb86652cb1805adfe47cb22
vickysmalls <55132051+vickysmalls@users.noreply.github.com>
2021-03-09 15:14:48 +0100

    feilmelding på infobox

--- src/oblig-fag/oblig-fag-semester.js
+++ src/oblig-fag/oblig-fag-semester.js
@@ -1,100 +1,71 @@
-import React, {useState} from 'react'
-import CustomButton from '../CustomButton/CustomButton';
-import InfoButton from '../CustomButton/InfoButton';
-import Modal from '../Modal/Modal';
-import useFetch from '../useFetch';
-import './oblig-fag.css';
-const _ = require("lodash");  
-
+import React, { useState } from "react";
+import CustomButton from "../CustomButton/CustomButton";
+import InfoButton from "../CustomButton/InfoButton";
+import Modal from "../Modal/Modal";
+import useFetch from "../useFetch";
+import "./oblig-fag.css";
+const _ = require("lodash");
 
 //Denne koomponenten gjør at man kan plotte inn semestesr og klasse id(answer) i oblig-fag-card
-const ObligFagSemester = ({answer, semester, setSemesterList7}) => {
+const ObligFagSemester = ({ answer, semester, setSemesterList7 }) => {
+  const { data: klassetrinn, error, isPending } = useFetch(
+    `http://localhost:5000/api/obligfag/`
+  );
 
-    
-    
-    const {data: klassetrinn, error, isPending} = useFetch(`http://localhost:5000/api/obligfag/`);
+  const [fagnavn, setFagnavn] = useState();
+  //const [semester, setSemester] = useState();
+  const [emnekode, setEmneKode] = useState();
+  const [studiepoeng, setStudiepoeng] = useState();
+  const [klasseId, setKlasseId] = useState();
+  const [isOpen, setIsOpen] = useState(false);
 
+  //lager et array som sortrer etter semester
+  const iteratees = (obj) => obj.semester;
+  const sorted = _.sortBy(klassetrinn, iteratees);
 
-    const [fagnavn, setFagnavn] = useState();
-    //const [semester, setSemester] = useState();
-    const [emnekode, setEmneKode] = useState();
-    const [studiepoeng, setStudiepoeng] = useState();
-    const [klasseId ,setKlasseId] = useState();
-    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <>
+        {
+          // det sorterte arrayet mappes
+          sorted.map(
+            (oblig) =>
+              //om klasseId er det samme som answer fra KlasseList =>
+              oblig.klasseId === answer &&
+              oblig.semester === semester && (
+                <>
+                  <CustomButton key={oblig.id}>{oblig.fagnavn}</CustomButton>
 
-    
+                  <InfoButton
+                    className="infoknapp"
+                    onClick={() => {
+                      setIsOpen(true);
+                      setFagnavn(oblig.fagnavn);
+                      //setSemester(oblig.semester)
+                      setEmneKode(oblig.emnekode);
+                      setStudiepoeng(oblig.studiepoeng);
+                      setKlasseId(oblig.klasseId);
+                      
+                    }}
+                  ></InfoButton>
+                </>
+              )
+          )
+        }
+      </>
 
-    //lager et array som sortrer etter semester
-    const iteratees = obj => obj.semester;
-    const sorted = _.sortBy(klassetrinn, iteratees);
-    
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        <h3>Info om {fagnavn}</h3>
+        <br />
 
-    return ( 
-        
-        <>
-        <>
-         {
-                
-                    // det sorterte arrayet mappes
-                    sorted.map((oblig)=>(
-                        //om klasseId er det samme som answer fra KlasseList =>
-                        oblig.klasseId ===answer &&
-                        oblig.semester ===semester &&
-                        <>
-
-                        <CustomButton 
-                            key={oblig.id}
-                        >
-
-                         {oblig.fagnavn}
-                        </CustomButton>
-
-                        <InfoButton className='infoknapp'
-                            onClick={() =>{ 
-                                setIsOpen(true)
-                                setFagnavn(oblig.fagnavn)
-                                //setSemester(oblig.semester)
-                                setEmneKode(oblig.emnekode)
-                                setStudiepoeng(oblig.studiepoeng)
-                                setKlasseId(oblig.klasseId)
-                                setSemesterList7(setSemesterList7)
-                            }}> 
-                        </InfoButton>                        
-                      </>  
-                    ))
-                    
-            }
-        </>
-        
-
-            <Modal 
-                open= {isOpen} 
-                onClose={() => setIsOpen(false)}
-            >
-                <h3>Info om {fagnavn}</h3><br/>
-
-                <ul>
-                    <li>
-                    Fagnavn: {fagnavn}
-                    </li>
-                    <li>
-                    Emnekode: {emnekode}
-                    </li>
-                    <li>
-                    Studiepoeng: {studiepoeng}
-                    </li>
-                    <li>
-                    klassetrinn: {klasseId}
-                    </li>
-                </ul>
-            </Modal>
-
-        </>
-        
-        
-        
-        
-     );
-     
+        <ul>
+          <li>Fagnavn: {fagnavn}</li>
+          <li>Emnekode: {emnekode}</li>
+          <li>Studiepoeng: {studiepoeng}</li>
+          <li>klassetrinn: {klasseId}</li>
+        </ul>
+      </Modal>
+    </>
+  );
 };
 export default ObligFagSemester;
